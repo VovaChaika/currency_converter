75f2ffa38d14b357e86b3749799ab5ea441c78bb
VovaChaika <75807498+VovaChaika@users.noreply.github.com>
2022-07-30 23:03:37 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ export const instance = axios.create({
     withCredentials: true,
     baseURL: `https://api.apilayer.com/fixer/`,
     headers: {
-        "apikey": "SrtNawm1Gau96BiHthkdIpr0runoK1Eb"
+        "apikey": "0emquFaiJaYOE2wfU38Kq4oy2xLkHWta"
     }
 })
 
